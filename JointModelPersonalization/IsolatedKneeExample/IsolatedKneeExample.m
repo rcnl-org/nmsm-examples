31bbaa4cda6a0ea7f4b4e0d084f8b709931bae19
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2022-02-25 16:31:49 -0600

    added calculating improvement

--- JointModelPersonalization/IsolatedKneeExample/IsolatedKneeExample.m
+++ JointModelPersonalization/IsolatedKneeExample/IsolatedKneeExample.m
@@ -2,3 +2,15 @@
 % Make sure the nmsm-core project is open by double-clicking Project.prj in
 % the nmsm-core repository.
 JointModelPersonalizationTool('IsolatedReinboltKneeExample.xml')
+
+% calculate improvement in marker error for markers on distal and proximal
+% bodies
+start = sqrt(calculateJointError('Rajagopal_4.0_RCNL_markers_scaled.osim', ...
+    1, jointArray, markerFile, 1e-6))
+
+final = sqrt(calculateJointError('Rajagopal_4.0_RCNL_markers_scaled_knee.osim', ...
+    1, jointArray, markerFile, 1e-6))
+
+improvement = (final - start) / start
+
+% use plotBeforeAndAfterMarkerError.m example to visualize difference
