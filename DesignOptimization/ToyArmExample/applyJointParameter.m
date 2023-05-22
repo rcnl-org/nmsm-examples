344876671830fb50d1443f9428898fd79f8fe9d2
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2023-05-22 14:31:10 -0500

    new temp xml file syntax

---
+++ DesignOptimization/ToyArmExample/applyJointParameter.m
@@ -0,0 +1,9 @@
+function inputs = applyJointParameter(inputs, values)
+value = values.elbow_translation;
+jointName = "r_elbow";
+coordinateNumber = 0;
+inputs.auxdata.model = Model(inputs.auxdata.model);
+adjustParentTranslation( ...
+    inputs.auxdata.model, jointName, coordinateNumber, value);
+end
+
