6fabef2dfcd5bc915efc4273fc15ad226c2e6d5c
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2023-05-24 15:09:15 -0500

    updated DO xml files

--- DesignOptimization/ToyArmExample/DOToyExample.m
+++ DesignOptimization/ToyArmExample/DOToyExample.m
@@ -1,13 +1,3 @@
+
 clc; clear;
 DesignOptimizationTool("TorqueDrivenDOSettings.xml")
-% settingsFileName = "TorqueDrivenDOSettings.xml";
-% settingsTree = xml2struct(settingsFileName);
-% [inputs, params] = parseDesignOptimizationSettingsTree(settingsTree);
-% inputs.costTerms
-% [outputs, inputs] = DesignOptimization(inputs, params);
-% function output = modifyJointParameter(inputs)
-% inputs.model.getJointSet()...
-% 
-% end
-% 
-% str2func("modifyJointParameter")
