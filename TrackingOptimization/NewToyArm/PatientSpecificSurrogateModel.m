77f4ea1198be46dbd916c829f76b7b94bfd6cfcd
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2023-07-01 21:04:20 -0500

    synergy driven to results

--- TrackingOptimization/NewToyArm/PatientSpecificSurrogateModel.m
+++ TrackingOptimization/NewToyArm/PatientSpecificSurrogateModel.m
@@ -5,23 +5,23 @@ switch numMuscles
     case 1
         theta1 = jointAngles(:, 1);
         thetaDot1 = jointVelocities(:, 1);
-        muscleTendonLengths = [ 6.*theta1, 15.*theta1.^2, 20.*theta1.^3, 15.*theta1.^4, 6.*theta1.^5, theta1.^6, onesCol,];
-        muscleTendonVelocities = [ 12.*thetaDot1, 60.*theta1.*thetaDot1, 120.*theta1.^2.*thetaDot1, 120.*theta1.^3.*thetaDot1, 60.*theta1.^4.*thetaDot1, 12.*theta1.^5.*thetaDot1, zeroCol,];
-        momentArms1 = [-6*onesCol, -30.*theta1, -60.*theta1.^2, -60.*theta1.^3, -30.*theta1.^4, -6.*theta1.^5, zeroCol,];
+        muscleTendonLengths = [ 5.*theta1, 10.*theta1.^2, 10.*theta1.^3, 5.*theta1.^4, theta1.^5, onesCol,];
+        muscleTendonVelocities = [ 10.*thetaDot1, 40.*theta1.*thetaDot1, 60.*theta1.^2.*thetaDot1, 40.*theta1.^3.*thetaDot1, 10.*theta1.^4.*thetaDot1, zeroCol,];
+        momentArms1 = [-5*onesCol, -20.*theta1, -30.*theta1.^2, -20.*theta1.^3, -5.*theta1.^4, zeroCol,];
         matrix = [muscleTendonLengths; muscleTendonVelocities;momentArms1; ];
     case 2
         theta1 = jointAngles(:, 1);
         thetaDot1 = jointVelocities(:, 1);
-        muscleTendonLengths = [ 6.*theta1, 15.*theta1.^2, 20.*theta1.^3, 15.*theta1.^4, 6.*theta1.^5, theta1.^6, onesCol,];
-        muscleTendonVelocities = [ 12.*thetaDot1, 60.*theta1.*thetaDot1, 120.*theta1.^2.*thetaDot1, 120.*theta1.^3.*thetaDot1, 60.*theta1.^4.*thetaDot1, 12.*theta1.^5.*thetaDot1, zeroCol,];
-        momentArms1 = [-6*onesCol, -30.*theta1, -60.*theta1.^2, -60.*theta1.^3, -30.*theta1.^4, -6.*theta1.^5, zeroCol,];
+        muscleTendonLengths = [ 5.*theta1, 10.*theta1.^2, 10.*theta1.^3, 5.*theta1.^4, theta1.^5, onesCol,];
+        muscleTendonVelocities = [ 10.*thetaDot1, 40.*theta1.*thetaDot1, 60.*theta1.^2.*thetaDot1, 40.*theta1.^3.*thetaDot1, 10.*theta1.^4.*thetaDot1, zeroCol,];
+        momentArms1 = [-5*onesCol, -20.*theta1, -30.*theta1.^2, -20.*theta1.^3, -5.*theta1.^4, zeroCol,];
         matrix = [muscleTendonLengths; muscleTendonVelocities;momentArms1; ];
     case 3
         theta1 = jointAngles(:, 1);
         thetaDot1 = jointVelocities(:, 1);
-        muscleTendonLengths = [ 6.*theta1, 15.*theta1.^2, 20.*theta1.^3, 15.*theta1.^4, 6.*theta1.^5, theta1.^6, onesCol,];
-        muscleTendonVelocities = [ 12.*thetaDot1, 60.*theta1.*thetaDot1, 120.*theta1.^2.*thetaDot1, 120.*theta1.^3.*thetaDot1, 60.*theta1.^4.*thetaDot1, 12.*theta1.^5.*thetaDot1, zeroCol,];
-        momentArms1 = [-6*onesCol, -30.*theta1, -60.*theta1.^2, -60.*theta1.^3, -30.*theta1.^4, -6.*theta1.^5, zeroCol,];
+        muscleTendonLengths = [ 5.*theta1, 10.*theta1.^2, 10.*theta1.^3, 5.*theta1.^4, theta1.^5, onesCol,];
+        muscleTendonVelocities = [ 10.*thetaDot1, 40.*theta1.*thetaDot1, 60.*theta1.^2.*thetaDot1, 40.*theta1.^3.*thetaDot1, 10.*theta1.^4.*thetaDot1, zeroCol,];
+        momentArms1 = [-5*onesCol, -20.*theta1, -30.*theta1.^2, -20.*theta1.^3, -5.*theta1.^4, zeroCol,];
         matrix = [muscleTendonLengths; muscleTendonVelocities;momentArms1; ];
 end
 end
