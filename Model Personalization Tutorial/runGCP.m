f3f6bd2b183e2e31b05c06cf1d266933582fc29d
stwilliams333 <stwilliamsmail@gmail.com>
2023-07-10 01:11:07 -0500

    Reorient motion, grf data, fix GCP example setup

--- Model Personalization Tutorial/runGCP.m
+++ Model Personalization Tutorial/runGCP.m
@@ -1,15 +1,15 @@
 %% Run GCP
-
+tic
 GroundContactPersonalizationTool(fullfile("Settings files", "GCPSettings.xml"))
-
+toc
 %% Analyze results
 
 close all
 
 plotGcpFootKinematicsFromFiles( ...
-    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot1_experimentalFootKinematics.sto"), ...
-    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot1_optimizedFootKinematics.sto"), 1)
+    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot_1_experimentalFootKinematics.sto"), ...
+    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot_1_optimizedFootKinematics.sto"), 1)
 
 plotGcpGroundReactionsFromFiles( ...
-    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot1_replacedExperimentalGroundReactions.sto"), ...
-    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot1_optimizedGroundReactions.sto"), 2)
+    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot_1_replacedExperimentalGroundReactions.sto"), ...
+    fullfile("gcpResults", "UF_Subject_3_reduced_muscles_Foot_1_optimizedGroundReactions.sto"), 2)
